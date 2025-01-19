524ac34853822a3efedeae2a4034944fc5210ffc
Katrina Orevillo (She/Her) <58285120+korevillo@users.noreply.github.com>
2025-01-18 20:32:08 -0800

    added homepage illustrations :3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+import allOnions from './images/all_onions.png';
+import top4 from './images/top_4.png';
+import bottom4 from './images/bottom_4.png';
 
 function App() {
   return (
     <div className='center-horizontal'>
       <div className='center-vertical'>
-        {/* Insert image */}
-        <h1 className='welcome-sign'>Welcome to</h1>
-        <h1>Onion City Grocery</h1>
-        <p>We've got a special deal on <span style={{fontWeight:'bold', color:'#4EAB10'}}>onions</span> today.
-          Pick your other groceries first, 
-          then we'll give you your special onion!</p>
-        <button>Let's go!</button> {/* Add handler */}
+        <img className='border-img' src={top4} alt=""/>
+
+        <div className='text-content'>
+          <h1 className='welcome-sign'>Welcome to</h1>
+          <h1>Onion City Grocery</h1>
+          <p>We've got a special deal on <span style={{fontWeight:'bold', color:'#4EAB10'}}>onions</span> today.
+            Pick your other groceries first, 
+            then we'll give you your special onion!</p>
+          <button className='start-button'>Let's go!</button> {/* Add handler */}
+        </div>
+        
+        <img className='border-img' src={bottom4} alt=""/>
       </div>
       </div>
   );
